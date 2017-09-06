5cfc737b1d6b17a4172b4c5034741530c90d9f42
jee00609 <31675804+jee00609@users.noreply.github.com>
2017-09-06 11:04:22 +0900

    It is my Homework
    열심히 하겠습니다

---
+++ Home-work.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+  
+	printf("1.제 학번은 2017301009 입니다\n");
+	printf("2.제 이름은 김민지입니다\n");
+	printf("3.제 전화번호는 010-5098-7349 입니다\n");
+	printf("4.제 깃허브 주소는 https://github.com/jee00609 입니다\n");
+
+ return 0;
+
+}
